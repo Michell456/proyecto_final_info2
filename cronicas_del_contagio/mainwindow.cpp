65066b41658a19f33d66c99c82523238d7f4ef3c
manueljmontoya <manuelj.montoya@udea.edu.co>
2025-11-23 00:59:18 -0500

    manejo de niveles en mainwindow y clase abstracta nivel

--- cronicas_del_contagio/mainwindow.cpp
+++ cronicas_del_contagio/mainwindow.cpp
@@ -1,11 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QKeyEvent>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    cargarMenuPrincipal();
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, [this](){
+        nivelActual->update();
+        update();   // vuelve a dibujar
+    });
+
+    timer->start(16); // 60 FPS
+}
+
+void MainWindow::cargarMenuPrincipal() {
+    delete nivelActual;
+    nivelActual = new menuPrincipal();
+}
+
+void MainWindow::cargarNivel1() {
+    delete nivelActual;
+    nivelActual = new NivelPesteNegra();
+}
+
+void MainWindow::cargarNivel2() {
+    delete nivelActual;
+    nivelActual = new NivelColera();
+}
+
+void MainWindow::cargarNivel3() {
+    delete nivelActual;
+    nivelActual = new NivelCovid();
+}
+
+void MainWindow::paintEvent(QPaintEvent *) {
+    QPainter p(this);
+    nivelActual->draw(p);
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event) {
+    nivelActual->handleInput(event);
 }
 
 MainWindow::~MainWindow()
--- cronicas_del_contagio/mainwindow.h
+++ cronicas_del_contagio/mainwindow.h
@@ -2,22 +2,56 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <QKeyEvent>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
+namespace Ui {class MainWindow;}
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow{
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void actualizarAnimacion();
+    void actualizarMovimiento();
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    QGraphicsPixmapItem *fig;
+
+    Nivel *nivelActual;
+    QTimer *timer;
+
+    void cargarMenuPrincipal();
+    void cargarNivel1();
+    void cargarNivel2();
+    void cargarNivel3();
+
+    // Variables para animación
+    QPixmap spriteSheet;
+    int frameWidth;
+    int frameHeight;
+    int frameActual;
+    int totalFrames;
+    QTimer *timerAnimacion;
+    QTimer *timerMovimiento;
+    bool moviendoseDerecha;
+    bool moviendoseIzquierda;
+    bool moviendoseArriba;
+    bool moviendoseAbajo;
+    int velocidad;
+    int filaAnimacion;
 };
 #endif // MAINWINDOW_H
+
